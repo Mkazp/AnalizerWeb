b27bced6e06289cafe9f303fe316ed0babc03a3a
Hurma47 <mkazp111@mail.ru>
2023-11-21 23:02:37 +0300

    firstlvl

---
+++ src/components/Menu.js
@@ -0,0 +1,32 @@
+import './App.scss';
+
+function Menu() {
+    return (
+      <div className='wrapper'>
+        <header>
+          <div className='TittleName'>
+            <h1>
+              Kat1
+            </h1>
+          </div>
+          <div className='FAQ'>
+            <h3>
+              FAQ
+            </h3>
+          </div>
+          <div className='VirtualDev'>
+            <h3>
+              Kat2
+            </h3>
+          </div>
+          <div className='Autorization'>
+            <h3>
+              Аккакунт
+            </h3> 
+          </div>
+        </header>
+      </div>
+    );
+  }
+
+export default Menu;
